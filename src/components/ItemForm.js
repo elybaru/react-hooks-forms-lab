d66d6752beea0a00ba3bfb5796449dff2b5800f1
Ely Karr <elykarr@gmail.com>
2021-11-30 11:04:30 -0800

    commit tree b8390fd2ed9fb77dc6427ef7c3bafb176b08e887

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
+
+  // set item name, set the item category
+
+  function handleItemName(event) {
+    setItemName(event.target.value)
+    console.log(itemName)
+  }
+
+  function handleItemCategory(event) {
+    setItemCategory(event.target.value)
+    console.log(itemCategory)
+  }
+
+  function handleNewItemSubmit(event) {
+    event.preventDefault();
+    const newItem = {
+      id: uuid(), // the `uuid` library can be used to generate a unique id
+      name: itemName,
+      category: itemCategory,
+    }
+    console.log(newItem)
+    onItemFormSubmit(newItem)
+
+  }
+
   return (
-    <form className="NewItem">
+    <form onSubmit={handleNewItemSubmit} className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input onChange={handleItemName} value={itemName} type="text" name="name" />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select onChange={handleItemCategory} value={itemCategory} name="category">
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
